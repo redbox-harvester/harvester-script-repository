8eaac888a23482c25ed18d2fbed5135d681aec29
Shilo Banihit <shiloworks@gmail.com>
2015-09-23 14:00:11 +1000

    Fixed template, added mapping of location and coverage.

--- inputs/oaipmhrif-redboxdataset/resources/scripts/jsonRifToRbDs.groovy
+++ inputs/oaipmhrif-redboxdataset/resources/scripts/jsonRifToRbDs.groovy
@@ -20,23 +20,25 @@ def metadata = record.metadata
 def templateJson = new JsonSlurper().parse(new File(config.harvest.oaiPmh.redbox.datasetTemplatePath))
 def workflowMeta = templateJson.data.data['workflow.metadata']
 
-def srcDf = "yyyy-MM-dd'T'mm:hh:ss"
+def srcDf = "yyyy-MM-dd'T'HH:mm:ss"
 def targetDf = "yyyy-MM-dd"
+def rbVersion = config.harvest.oaiPmh.redbox.version
   
 templateJson.data.data.with {
   def regObj = metadata.registryObjects.registryObject
   def identifier = record.header.identifier
   // Basics
   datasetId = identifier.substring(identifier.lastIndexOf('/') + 1)
-  workflowMeta.formData.description =  tfpackage.description = tfpackage['dc:description'] =  regObj.collection.description.$
+   workflowMeta.formData.description =  tfpackage.description = tfpackage['dc:description'] =  regObj.collection.description.$
   workflowMeta.formData.title = tfpackage.title = tfpackage['dc:title'] = regObj.collection.name.namePart
+  tfpackage['redbox:formVersion'] = rbVersion
   // Dates
-  tfpackage['dc:created'] = Date.parse(srcDf, regObj.collection.dateAccessioned).format(targetDf)
+  tfpackage['dc:created'] = convertDate(srcDf, targetDf, regObj.collection.dateAccessioned)
   if (regObj.collection.dateModified) {
-    tfpackage['dc:modified'] = Date.parse(srcDf, regObj.collection.dateModified).format(targetDf)
+    tfpackage['dc:modified'] = convertDate(srcDf, targetDf, regObj.collection.dateModified)
   }
   // Location
-  if (regObj.collection.location.address) {
+  if (regObj.collection.location?.address) {
     int addressCtr = 1
     if (isCollectionOrArray(regObj.collection.location.address)) {
       regObj.collection.location.address.each {
@@ -46,9 +48,15 @@ templateJson.data.data.with {
       addAddress(addressCtr, regObj.collection.location.address, tfpackage)
     }
   }
-  // Coverage
-  
-  // Geospatial
+  // Coverage 
+  int spatialCtr = 1
+  if (isCollectionOrArray(regObj.collection.coverage)) {
+    regObj.collection.coverage.each {coverageObj->
+      spatialCtr = addCoverage(spatialCtr, coverageObj, tfpackage, srcDf, targetDf)
+    }
+  } else {
+    spatialCtr = addCoverage(spatialCtr, regObj.collection.coverage, tfpackage, srcDf, targetDf)
+  }
   
   // Relations
   
@@ -67,8 +75,11 @@ templateJson.data.data.with {
   
 }
 
-return JsonOutput.prettyPrint(JsonOutput.toJson(templateJson))
+return JsonOutput.toJson(templateJson)
 
+/* *******************************************************************/
+/* Helper methods 
+/* *******************************************************************/
 
 boolean isCollectionOrArray(object) {    
   [Collection, Object[]].any { it.isAssignableFrom(object.getClass()) }
@@ -82,4 +93,59 @@ def addAddress(ctr, addressObj, tfpackage) {
     ctr++
   }
   return ctr
+}
+
+def convertDate(fromDf, toDf, dateStr) {
+  return Date.parse(fromDf, dateStr).format(toDf) 
+}
+
+def addCoverage(spatialCtr, coverageObj, tfpackage, srcDf, targetDf) {
+  def literal = coverageObj?.temporal?.text ? coverageObj?.temporal?.text : coverageObj?.spatial?.$
+  if (literal) {
+    // Temporal
+    if (coverageObj?.temporal?.text) 
+      tfpackage['dc:coverage.redbox:timePeriod'] = "${literal}"
+    if (coverageObj.temporal?.date) { 
+      // having 2 dates is not guaranteed
+      if (isCollectionOrArray(coverageObj.temporal.date)) {
+        coverageObj.temporal.date.each { dtObj ->
+          addTemporalDate(dtObj, srcDf, targetDf, tfpackage)
+        }
+      } else {
+        // single dates
+        addTemporalDate(coverageObj.temporal.date, srcDf, targetDf, tfpackage)
+      }
+    }
+    // Spatial
+    if (coverageObj.spatial) {
+      if (isCollectionOrArray(coverageObj.spatial)) {
+        coverageObj.spatial.each { spatialObj ->
+          spatialCtr = addSpatial(spatialCtr, spatialObj, tfpackage)
+        }
+      } else {
+        spatialCtr = addSpatial(spatialCtr, coverageObj.spatial, tfpackage)
+      }
+    }
+  }
+  return spatialCtr
+}
+
+def addTemporalDate(dtObj, srcDf, targetDf, tfpackage) {
+  if (dtObj.type == 'dateFrom')
+    tfpackage['dc:coverage.vivo:DateTimeInterval.vivo:start'] = convertDate(srcDf, targetDf, dtObj.$)
+  else if (dtObj.type == 'dateTo')
+    tfpackage['dc:coverage.vivo:DateTimeInterval.vivo:end'] = convertDate(srcDf, targetDf, dtObj.$)
+}
+
+def addSpatial(ctr, spatialObj, tfpackage) {
+  tfpackage['dc:coverage.vivo:GeographicLocation.'+ctr+'.dc:type'] = spatialObj.type
+  if (spatialObj.type == 'dcmiPoint') {
+    def arr = spatialObj.$.split(';')
+    tfpackage['dc:coverage.vivo:GeographicLocation.'+ctr+'.rdf:PlainLiteral'] = arr[0]
+    tfpackage['dc:coverage.vivo:GeographicLocation.'+ctr+'.geo:long'] = arr[1].split('=')[1]
+    tfpackage['dc:coverage.vivo:GeographicLocation.'+ctr+'.geo:lat'] = arr[2].split('=')[1]
+  } else {
+    tfpackage['dc:coverage.vivo:GeographicLocation.'+ctr+'.rdf:PlainLiteral'] = spatialObj.$
+  }
+  return ++ctr
 }
